88517db586b689cbef1c50d2096f75a9c937be69
benjaminran <benjaminran2@gmail.com>
2015-07-16 21:07:27 -0700

    added half-formed unit tests

---
+++ app/src/test/java/com/bran/smarttuner/PitchTest.java
@@ -0,0 +1,46 @@
+package com.bran.smarttuner;
+
+import com.bran.smarttuner.Pitch;
+import org.junit.Test;
+
+/**
+ * Created by beni on 7/16/15.
+ */
+public class PitchTest {
+
+        @Test
+        public void testFromFrequency() throws Exception {
+            Pitch pitch = Pitch.fromFrequency(440f);
+            assert(pitch.getOctaveNumber()==5);
+            assert(pitch.getNoteName().equals("C"));
+            assert(pitch.getCentsSharp()==0);
+        }
+
+        public void testGetNote() throws Exception {
+
+        }
+
+        public void testGetCentsSharp() throws Exception {
+
+        }
+
+        public void testToString() throws Exception {
+
+        }
+
+        public void testGetNoteName() throws Exception {
+
+        }
+
+        public void testGetOctaveNumber() throws Exception {
+
+        }
+
+        public void testGetNoteName1() throws Exception {
+
+        }
+
+        public void testGetOctaveNumber1() throws Exception {
+
+        }
+    }
